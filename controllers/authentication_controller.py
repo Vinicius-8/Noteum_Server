08e3429387b157e94e2dbe2d4f50709822db5695
Vinicius <viniciusvieiradearaujo@hotmail.com>
2020-06-07 21:01:08 -0300

    heroku test: env variable

--- controllers/authentication_controller.py
+++ controllers/authentication_controller.py
@@ -1,11 +1,11 @@
 from google.oauth2 import id_token
 from google.auth.transport import requests
-import credentials
+from os import environ
 
 
 def auth_token(token) -> dict:
     try:
-        idinfo = id_token.verify_oauth2_token(token, requests.Request(), credentials.ANDROID_CLIENT_ID)
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), environ.get('ANDROID_CLIENT_ID'))
         if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
             print('O login não foi autenticado pelo servidor')
             raise ValueError('Wrong issuer')
